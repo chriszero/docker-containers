a53bff8ab7136a2b41793fb71e03e6b1d287549c
chriszero <zerov83@gmail.com>
2015-09-26 18:45:29 +0200

    Custom base image

--- vdr4arch/README.md
+++ vdr4arch/README.md
@@ -1,6 +1,18 @@
-Docker image for vdr based on vdr4arch
-
-Configure it via the volumes mounted on your host
+# Docker image for vdr based on vdr4arch  
+VDR container based on vdr4arch with minimal systemd
 
 Start it by running:
-docker run -d --privileged --name=vdr4arch --net=host -v /vdretc/:/etc/vdr -v /vdrconf/conf/:/var/lib/vdr -v /records/:/srv/vdr/video -v /etc/localtime:/etc/localtime:ro chriszero/vdr4arch
+`docker run -d --net=host --privileged --name=<containername> \
+-v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+-v /etc/localtime:/etc/localtime:ro \
+-v /<recordings_on_host>/:/srv/vdr/video \
+chriszero/vdr4arch`
+
+
+
+* --net=host				for have a working sat>ip plugin (UPNP)
+* -v /sys/fs/cgroup:/sys/fs/cgroup:ro	requirement for systemd
+
+To config your VDR attach a bash to the running container e.g: docker exec -it <containername> /bin/bash -c "export TERM=xterm; exec bash"
+
+
--- vdr4arch/setup.sh
+++ vdr4arch/setup.sh
@@ -5,24 +5,17 @@ echo 'de_DE.UTF-8 UTF-8' >> /etc/locale.gen
 locale-gen
 echo 'LANG=de_DE.UTF-8' > /etc/locale.conf
 source /etc/profile
-useradd -u 1000 -r -s /usr/bin/nologin vdr && usermod -aG log,disk,bin vdr
+#useradd -u 1000 -r -s /usr/bin/nologin vdr && usermod -aG log,disk,bin vdr
 	
 pacman -U --noconfirm vdr4arch-keyring-20130219-1-any.pkg.tar.xz
 pacman-key --init
 pacman-key --populate vdr4arch
 
 printf '%s\n%s' '[vdr4arch]' 'Server = http://creimer.net/vdr4arch/repos/$repo/os/$arch' >> /etc/pacman.conf
-pacman -Sy 
-pacman -S --noconfirm nano vdr vdr-satip vdr-live vdr-dvbapi vdr-epg2vdr vdr-vnsiserver vdr-restfulapi vdr-streamdev-server vdr-epgsearch vdr-remotetimers vdr-dummydevice vdr-wirbelscan 
+pacman -Sy --noconfirm nano vdr vdr-satip vdr-live vdr-dvbapi vdr-epg2vdr vdr-vnsiserver vdr-restfulapi vdr-streamdev-server vdr-epgsearch vdr-remotetimers vdr-dummydevice vdr-wirbelscan 
 pacman -Scc --noconfirm
 
-
-ln -s /etc/vdr/conf.avail/50-dvbapi.conf /etc/vdr/conf.d/
-ln -s /etc/vdr/conf.avail/50-live.conf /etc/vdr/conf.d/
-ln -s /etc/vdr/conf.avail/50-satip.conf /etc/vdr/conf.d/
-ln -s /etc/vdr/conf.avail/50-wirbelscan.conf /etc/vdr/conf.d/
-ln -s /etc/vdr/conf.avail/50-dummydevice.conf /etc/vdr/conf.d/
-mkdir -p /srv/vdr/video && chown -R vdr:vdr /srv/vdr/video
+mkdir -p /srv/vdr/video
 
 mv channels.conf /var/lib/vdr/channels.conf
 chown vdr /var/lib/vdr/channels.conf
