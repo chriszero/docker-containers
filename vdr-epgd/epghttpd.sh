0b84b4a839f9a2961f81e6aa54618988e1f493f1
chriszero <zerov83@gmail.com>
2015-12-28 14:54:35 +0100

    vdr-epgd http branch, for testing only

---
+++ vdr-epgd/epghttpd.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# `/sbin/setuser memcache` runs the given command as the user `memcache`.
+# If you omit that part, the command will be run as root.
+export LANG="de_DE.UTF-8"
+exec /usr/local/bin/epghttpd -n -c /config
---
+++ vdr-epgd/setup_epgd.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+locale-gen de_DE.UTF-8
+export LANG="de_DE.UTF-8"  
+export LANGUAGE="de_DE:de"  
+export LC_ALL="de_DE.UTF-8"  
+
+apt-get update -qq
+apt-get install -qy build-essential \
+	git \
+	wget unzip \
+	uuid uuid-dev \
+	libarchive13 libarchive-dev \
+	zlib1g zlib1g-dev \
+	libcurl3-nss libcurl4-nss-dev \
+	libxslt1.1 libxslt1-dev \
+	libxml2 libxml2-dev \
+	libmysqlclient-dev libmysqlclient18 \
+	libjpeg8 libjpeg-dev libtiff-dev \
+	libimlib2 libimlib2-dev \
+	libjansson4 libjansson-dev \
+	libssl-dev \
+	libmicrohttpd10 libmicrohttpd-dev \
+	libimlib2 libimlib2-dev \
+	libjpeg8 libjpeg-dev \
+	python libpython2.7 libpython-dev python-dev \
+	mailutils
+
+
+
+# Compile and install
+mkdir -p /usr/lib/mysql/plugin
+mkdir -p /config
+cd /tmp && \
+	git clone -b http git://projects.vdr-developer.org/vdr-epg-daemon.git && \
+	cd vdr-epg-daemon && \
+	patch -p1 < ../tvm-http.diff && \
+	make all install
+cp /etc/epgd/* /config
+
+# Clean up APT when done.
+apt-get remove -qy \
+	build-essential \
+	wget \
+	uuid-dev \
+	libarchive-dev \
+	zlib1g-dev \
+	libcurl4-nss-dev \
+	libxslt1-dev \
+	libxml2-dev \
+	libmysqlclient-dev \
+	libjpeg-dev libtiff-dev \
+	libimlib2-dev \
+	libjansson-dev \
+	libssl-dev \
+	libjansson-dev \
+	libmicrohttpd-dev \
+	libpython-dev python-dev
+
+apt-get autoremove -qy && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
